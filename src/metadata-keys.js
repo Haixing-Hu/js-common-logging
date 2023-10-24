74ff739da67887747f6312d0326ed421c438d3d4
Haixing Hu <starfish.hu@gmail.com>
2023-10-24 11:56:40 +0800

    docs: change the style of copyright comments

--- src/metadata-keys.js
+++ src/metadata-keys.js
@@ -1,11 +1,11 @@
-/*******************************************************************************
- *
- *    Copyright (c) 2022 - 2023.
- *    Haixing Hu, Qubit Co. Ltd.
- *
- *    All rights reserved.
- *
- ******************************************************************************/
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2022 - 2023.
+//    Haixing Hu, Qubit Co. Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
 const LOGGER_KEY = '__common_logging_logger__';
 const VUE3_CLASS_COMPONENT_DECORATORS_KEY = '__vue3_class_component_decorators__';
 
