9fc04246212882e39133d1a64b3fc05db7f58439
Haixing Hu <starfish.hu@gmail.com>
2023-12-08 11:36:10 +0800

    build: upgrade dependencies, and rename the package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,4 +8,4 @@
 ////////////////////////////////////////////////////////////////////////////////
 import rollupBuilder from '@haixing_hu/rollup-builder';
 
-export default rollupBuilder('CommonLogging', import.meta.url);
+export default rollupBuilder('Logging', import.meta.url);
